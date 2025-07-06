4f9e1327fe5b898a1629d978478bb6b6b412771b
remediesss <hellzerk17@gmail.com>
2025-07-06 19:56:42 +0300

    added getter

--- app/src/main/java/com/example/myapp/ComputerPaddle.kt
+++ app/src/main/java/com/example/myapp/ComputerPaddle.kt
@@ -1,7 +1,7 @@
 import android.view.View
 
 class ComputerPaddle(
-    private val view: View,  // View ракетки компьютера
+    private val computerPaddleView: View,  // View ракетки компьютера
     private val screenWidth: Int,
     private val ball: Ball,
     private val speed: Float = 15f
@@ -9,16 +9,20 @@ class ComputerPaddle(
     fun update() {
 
         val ballCenterX = ball.x + ball.width / 2f
-        val paddleCenterX = view.x + view.width / 2f
+        val paddleCenterX = computerPaddleView.x + computerPaddleView.width / 2f
 
         // Определяем направление движения
         when {
             paddleCenterX < ballCenterX -> {
-                view.x = (view.x + speed).coerceAtMost(screenWidth - view.width.toFloat())
+                computerPaddleView.x = (computerPaddleView.x + speed).coerceAtMost(screenWidth - computerPaddleView.width.toFloat())
             }
             paddleCenterX > ballCenterX -> {
-                view.x = (view.x - speed).coerceAtLeast(0f)
+                computerPaddleView.x = (computerPaddleView.x - speed).coerceAtLeast(0f)
             }
         }
+    }
+
+    fun getView(): View {
+        return computerPaddleView
     }
 }
