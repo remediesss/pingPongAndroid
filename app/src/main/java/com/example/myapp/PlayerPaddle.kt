e99a4aea832143dd4b98f2c49e7d307d830abd12
remediesss <hellzerk17@gmail.com>
2025-07-06 18:42:29 +0300

    add PlayerPaddle.kt & logic of player paddle movement

---
+++ app/src/main/java/com/example/myapp/PlayerPaddle.kt
@@ -0,0 +1,10 @@
+import android.view.View
+
+class PlayerPaddle(
+    private val playerPaddleView: View,  // View ракетки игрока
+    private val screenWidth: Int
+) {
+    fun move(newX: Float) {
+        playerPaddleView.x = newX.coerceIn(0f, screenWidth - playerPaddleView.width.toFloat())
+    }
+}
