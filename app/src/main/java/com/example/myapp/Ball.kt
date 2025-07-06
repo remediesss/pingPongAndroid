6a83caf3e5a3ec1e5782b073e3261675da900013
remediesss <hellzerk17@gmail.com>
2025-07-06 18:36:55 +0300

    add ball.kt & logic of ball movement

---
+++ app/src/main/java/com/example/myapp/Ball.kt
@@ -0,0 +1,37 @@
+import android.view.View
+
+class Ball(
+    private val ballView: View,  // View мяча из XML
+    private val screenWidth: Int,
+    private val screenHeight: Int
+) {
+    private var speedX = 10f
+    private var speedY = 10f
+
+    fun update() {
+        ballView.x += speedX
+        ballView.y += speedY
+
+        // Отскок от стен
+        if (ballView.x <= 0 || ballView.x >= screenWidth - ballView.width) {
+            speedX *= -1
+        }
+    }
+
+    fun reset() {
+        ballView.x = (screenWidth / 2 - ballView.width / 2).toFloat()
+        ballView.y = (screenHeight / 2 - ballView.height / 2).toFloat()
+        speedX = if (Math.random() < 0.5) 10f else -10f
+        speedY *= -1
+    }
+
+    fun checkCollision(paddle: View): Boolean {
+        return ballView.y + ballView.height >= paddle.y &&
+                ballView.x + ballView.width >= paddle.x &&
+                ballView.x <= paddle.x + paddle.width
+    }
+
+    fun reverseY() {
+        speedY *= -1
+    }
+}
